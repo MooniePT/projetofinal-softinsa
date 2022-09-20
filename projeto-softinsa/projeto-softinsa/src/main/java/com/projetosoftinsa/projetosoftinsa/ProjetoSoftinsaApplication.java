9468bd5f9246e92f54816b1c9f21270593312d3e
carlos.farinha <carlos.farinha@FormCS.softinsa.corp>
2022-09-20 09:29:28 +0100

    Inicio do projeto

---
+++ projeto-softinsa/projeto-softinsa/src/main/java/com/projetosoftinsa/projetosoftinsa/ProjetoSoftinsaApplication.java
@@ -0,0 +1,13 @@
+package com.projetosoftinsa.projetosoftinsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSoftinsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSoftinsaApplication.class, args);
+	}
+
+}
